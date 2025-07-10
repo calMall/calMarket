107a4b3794fdc631afbdee615e093cea955f29fe
if <ckf7997@gmail.com>
2025-07-10 17:18:44 +0900

    fix:new create

---
+++ src/main/java/com/example/calmall/CalmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.calmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calmall/CalmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
