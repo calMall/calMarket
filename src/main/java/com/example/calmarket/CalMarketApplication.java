ffa346c566f46f1a688e6e1b23913d3634448e72
if <ckf7997@gmail.com>
2025-07-14 14:40:00 +0900

    初回コミット

---
+++ src/main/java/com/example/calmarket/CalMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.calmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalMarketApplication.class, args);
+    }
+
+}
