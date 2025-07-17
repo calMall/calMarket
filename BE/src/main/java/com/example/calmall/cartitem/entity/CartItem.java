3ff1b0ec95f3bdb58ac7a575a82407ff4d45d8ef
if <ckf7997@gmail.com>
2025-07-17 11:06:31 +0900

    解決衝突：刪除 .idea/misc.xml

---
+++ BE/src/main/java/com/example/calmall/cartitem/entity/CartItem.java
@@ -0,0 +1,22 @@
+package com.example.calmall.cartitem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+// ユーザーのカート情報を管理する中間エンティティ
+@Entity
+@Data
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // カートアイテムID
+
+    private Integer userId; // 所属ユーザーID
+
+    private String itemCode; // 商品ID（楽天形式）
+
+    private Integer quantity; // 数量0になったら削除
+
+    private String option; // APIから与えられる商品オプション
+}
