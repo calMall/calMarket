0d44533c44091cbba897a68472a66f389527e38c
Kwon <kjr5876@gmail.com>
2025-08-22 17:18:01 +0900

    fix: skeleton ui 修正

--- fe/src/components/order/LoadingOrderBox.tsx
+++ fe/src/components/order/LoadingOrderBox.tsx
@@ -2,10 +2,10 @@ export default function LoadingOrderBox() {
   return (
     <div className="order-item bb mb-1 pb-1">
       <div className="flex gap-1">
-        <div className="loading-box order-loding" />
+        <div className="loading-box order-loading" />
         <div className="flex flex-col gap-05 wf">
-          <div className="loading-box order-loding-text" />
-          <div className="loading-box order-loding-text" />
+          <div className="loading-box order-loading-text" />
+          <div className="loading-box order-loading-text" />
         </div>
       </div>
     </div>
